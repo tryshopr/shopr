baea1c7f9481e51c55d11b9710f626b254f41450
Adam Cooke <adam@atechmedia.com>
2013-10-17 19:00:07 +0100

    further test prepartion

--- test/models/shoppe/user_test.rb
+++ test/models/shoppe/user_test.rb
@@ -3,34 +3,24 @@ require 'test_helper'
 module Shoppe
   class UserTest < ActiveSupport::TestCase
     
-    setup do
-      @user = User.first
-    end
-    
     test "authentication" do
-      user = User.authenticate('adam@niftyware.io', 'llamafarm')
-      assert_equal User, user.class
-      user = User.authenticate('adam@niftyware.io', 'invalid')
-      assert_equal false, user
+      user = User.create(:email_address => 'adam@niftyware.io', :password => 'llamafarm', :password_confirmation => 'llamafarm', :first_name => 'Adam', :last_name => 'Cooke')
+
+      authed_user = User.authenticate('adam@niftyware.io', 'llamafarm')
+      assert_equal User, authed_user.class
+      assert_equal user, authed_user
+      authed_user = User.authenticate('adam@niftyware.io', 'invalid')
+      assert_equal false, authed_user
     end
     
     test "full name" do
-      assert_equal "Adam Cooke", @user.full_name
+      user = User.new(:first_name => 'Adam', :last_name => 'Cooke')
+      assert_equal "Adam Cooke", user.full_name
     end
     
     test "short name" do
-      assert_equal "Adam C", @user.short_name
-    end
-    
-    test "user creation" do
-      user = User.new
-      assert_equal false, user.save
-      user.first_name = 'Test'
-      user.last_name = 'User'
-      user.email_address = 'test@example.com'
-      user.password = 'password'
-      user.password_confirmation = 'password'
-      assert_equal true, user.save
+      user = User.new(:first_name => 'Adam', :last_name => 'Cooke')
+      assert_equal "Adam C", user.short_name
     end
     
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,8 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
+
+if Shoppe::Product.all.empty?
+  puts "Loading Shoppe seed data as database seems to be empty..."
+  Shoppe::Engine.load_seed
+end
