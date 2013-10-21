49cd3bd45d5ab4a6da16d2b55160b89ad7bb7ac0
Adam Cooke <adam@atechmedia.com>
2013-10-21 18:20:26 +0100

    mailer into own module

--- app/mailers/shoppe/order_mailer.rb
+++ app/mailers/shoppe/order_mailer.rb
@@ -1,25 +1,27 @@
-class Shoppe::OrderMailer < ActionMailer::Base
+module Shoppe
+  class OrderMailer < ActionMailer::Base
   
-  default :from => "#{Shoppe.config[:store_name]} <#{Shoppe.config[:email_address]}>"
+    default :from => "#{Shoppe.config[:store_name]} <#{Shoppe.config[:email_address]}>"
   
-  def received(order)
-    @order = order
-    mail :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.subject', :default => "Order Confirmation")
+    def received(order)
+      @order = order
+      mail :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.subject', :default => "Order Confirmation")
+    end
+  
+    def accepted(order)
+      @order = order
+      mail :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.accepted', :default => "Order Accepted")
+    end
+  
+    def rejected(order)
+      @order = order
+      mail :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.rejected', :default => "Order Rejected")
+    end
+  
+    def shipped(order)
+      @order = order
+      mail :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.shipped', :default => "Order Shipped")
+    end
+  
   end
-  
-  def accepted(order)
-    @order = order
-    mail :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.accepted', :default => "Order Accepted")
-  end
-  
-  def rejected(order)
-    @order = order
-    mail :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.rejected', :default => "Order Rejected")
-  end
-  
-  def shipped(order)
-    @order = order
-    mail :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.shipped', :default => "Order Shipped")
-  end
-  
 end
