8a8a1ade1dea9e0d16fc46e012d8b8bf8a4f67ee
Adam Cooke <adam@atechmedia.com>
2013-10-22 10:37:51 +0100

    update before to after

--- db/migrate/20131022090919_refactor_order_items_to_allow_any_product.rb
+++ db/migrate/20131022090919_refactor_order_items_to_allow_any_product.rb
@@ -1,6 +1,6 @@
 class RefactorOrderItemsToAllowAnyProduct < ActiveRecord::Migration
   def change
     rename_column :shoppe_order_items, :product_id, :ordered_item_id
-    add_column :shoppe_order_items, :ordered_item_type, :string, :before => :ordered_item_id
+    add_column :shoppe_order_items, :ordered_item_type, :string, :after => :ordered_item_id
   end
 end
