0a8dc52d1ec65ee746a0be35d0b3a1cdbfb26b32
Adam Cooke <adam@atechmedia.com>
2013-10-24 18:25:17 +0100

    do not allow settings to be changed in demo mode. closes #36

--- app/controllers/shoppe/settings_controller.rb
+++ app/controllers/shoppe/settings_controller.rb
@@ -4,6 +4,10 @@ module Shoppe
     before_filter { @active_nav = :settings }
     
     def update
+      if Shoppe.settings.demo_mode?
+        raise Shoppe::Error, "You cannot make changes to settings in demo mode. Sorry about that."
+      end
+      
       Shoppe::Setting.update_from_hash(params[:settings].permit!)
       redirect_to :settings, :notice => "Settings have been updated successfully."
     end
