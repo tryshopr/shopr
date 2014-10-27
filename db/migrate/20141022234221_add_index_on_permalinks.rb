19357852c80b8fb5fa7f4c5c4d0b384ed56aadb4
m-codebastards <m@codebastards.pl>
2014-10-27 10:23:31 +0100

    update indexes

--- db/migrate/20141022234221_add_index_on_permalinks.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexOnPermalinks < ActiveRecord::Migration
-  def change
-    add_index :shoppe_products, :permalink, unique: true
-    add_index :shoppe_product_categories, :permalink, unique: true
-  end
-end
