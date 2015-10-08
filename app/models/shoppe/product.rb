8d60384e0bc8cfb29392acf61409ef60f471f40e
Christoph Werner <christoph.werner@outlook.com>
2015-10-08 11:13:00 +0200

    Also update quantities if adjustment is the same as the current product stock

--- app/models/shoppe/product.rb
+++ app/models/shoppe/product.rb
@@ -159,7 +159,7 @@ module Shoppe
         # Don't import products where the name is blank
         unless row["name"].nil?
           if product = where(name: row["name"]).take
-            # Dont import products with the same name but update quantities if they're not the same
+            # Dont import products with the same name but update quantities
             qty = row["qty"].to_i
             if qty > 0 && qty != product.stock
               product.stock_level_adjustments.create!(description: I18n.t('shoppe.import'), adjustment: qty)
