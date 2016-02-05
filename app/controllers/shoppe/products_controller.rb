285916def2ef65a6f250331fa16b589980f2c59a
Yury Batenko <jurbat@gmail.com>
2016-02-05 15:14:59 +0700

    Implement products filtering by sku or name and description

--- app/controllers/shoppe/products_controller.rb
+++ app/controllers/shoppe/products_controller.rb
@@ -7,8 +7,25 @@ module Shoppe
     def index
       @products_paged = Shoppe::Product.root.
                   includes(:translations, :stock_level_adjustments, :product_categories, :variants).
-                  order(:name).
-                  page(params[:page])
+                  order(:name)
+      case
+      when params[:sku]
+        @products_paged = @products_paged.
+                          with_translations(I18n.locale).
+                          page(params[:page]).
+                          ransack(sku_cont_all: params[:sku].split).result
+      when params[:name]
+        @products_paged = @products_paged.
+                          with_translations(I18n.locale).
+                          page(params[:page]).
+                          ransack(translations_name_or_translations_description_cont_all: params[:name].split).
+                          result
+      else
+        @products_paged = @products_paged.page(params[:page])
+      end
+
+
+
       @products = @products_paged.
                   group_by(&:product_category).
                   sort_by { |cat,pro| cat.name }
