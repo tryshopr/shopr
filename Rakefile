87e5fb48d480240499bbbae080b68422c912089c
Dean Perry <dean@voupe.com>
2016-07-03 20:24:02 +0100

    removed rake test task

--- Rakefile
+++ Rakefile
@@ -4,19 +4,10 @@ rescue LoadError
   puts 'You must `gem install bundler` and `bundle install` to run rake tasks'
 end
 
-APP_RAKEFILE = File.expand_path('../test/app/Rakefile', __FILE__)
+APP_RAKEFILE = File.expand_path('../spec/dummy/Rakefile', __FILE__)
 load 'rails/tasks/engine.rake'
 
 Bundler::GemHelper.install_tasks
-
-require 'rake/testtask'
-Rake::TestTask.new(:test) do |t|
-  t.libs << 'lib'
-  t.libs << 'test'
-  t.pattern = 'test/**/*_test.rb'
-  t.verbose = false
-  t.warning = false
-end
 
 # namespace :shopr do
 #   desc 'Publish the release notes'
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20160608183749) do
 
-  create_table "nifty_key_value_store", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "nifty_key_value_store", force: :cascade do |t|
     t.integer  "parent_id"
     t.string   "parent_type"
     t.string   "group"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "shopr_addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_addresses", force: :cascade do |t|
     t.integer  "customer_id"
     t.string   "address_type"
     t.boolean  "default"
@@ -34,10 +34,10 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.integer  "country_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.index ["customer_id"], name: "index_shopr_addresses_on_customer_id", using: :btree
+    t.index ["customer_id"], name: "index_shopr_addresses_on_customer_id"
   end
 
-  create_table "shopr_attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_attachments", force: :cascade do |t|
     t.integer  "parent_id",   null: false
     t.string   "parent_type", null: false
     t.string   "token"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "shopr_countries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_countries", force: :cascade do |t|
     t.string   "name"
     t.string   "code2"
     t.string   "code3"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.datetime "updated_at",                 null: false
   end
 
-  create_table "shopr_customers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_customers", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "company"
@@ -73,24 +73,24 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "shopr_delivery_service_prices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_delivery_service_prices", force: :cascade do |t|
     t.integer  "delivery_service_id"
     t.string   "code"
-    t.decimal  "price",                             precision: 8, scale: 2
-    t.decimal  "cost_price",                        precision: 8, scale: 2
+    t.decimal  "price",               precision: 8, scale: 2
+    t.decimal  "cost_price",          precision: 8, scale: 2
     t.integer  "tax_rate_id"
-    t.decimal  "min_weight",                        precision: 8, scale: 2
-    t.decimal  "max_weight",                        precision: 8, scale: 2
-    t.text     "country_ids",         limit: 65535
-    t.datetime "created_at",                                                null: false
-    t.datetime "updated_at",                                                null: false
-    t.index ["delivery_service_id"], name: "index_shopr_delivery_service_prices_on_delivery_service_id", using: :btree
-    t.index ["max_weight"], name: "index_shopr_delivery_service_prices_on_max_weight", using: :btree
-    t.index ["min_weight"], name: "index_shopr_delivery_service_prices_on_min_weight", using: :btree
-    t.index ["price"], name: "index_shopr_delivery_service_prices_on_price", using: :btree
+    t.decimal  "min_weight",          precision: 8, scale: 2
+    t.decimal  "max_weight",          precision: 8, scale: 2
+    t.text     "country_ids"
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.index ["delivery_service_id"], name: "index_shopr_delivery_service_prices_on_delivery_service_id"
+    t.index ["max_weight"], name: "index_shopr_delivery_service_prices_on_max_weight"
+    t.index ["min_weight"], name: "index_shopr_delivery_service_prices_on_min_weight"
+    t.index ["price"], name: "index_shopr_delivery_service_prices_on_price"
   end
 
-  create_table "shopr_delivery_services", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_delivery_services", force: :cascade do |t|
     t.string   "name"
     t.string   "code"
     t.boolean  "default",      default: false
@@ -99,10 +99,10 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.string   "tracking_url"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
-    t.index ["active"], name: "index_shopr_delivery_services_on_active", using: :btree
+    t.index ["active"], name: "index_shopr_delivery_services_on_active"
   end
 
-  create_table "shopr_order_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_order_items", force: :cascade do |t|
     t.integer  "order_id"
     t.integer  "ordered_item_id"
     t.string   "ordered_item_type"
@@ -114,11 +114,11 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.decimal  "weight",            precision: 8, scale: 3
     t.datetime "created_at",                                            null: false
     t.datetime "updated_at",                                            null: false
-    t.index ["order_id"], name: "index_shopr_order_items_on_order_id", using: :btree
-    t.index ["ordered_item_id", "ordered_item_type"], name: "index_shopr_order_items_ordered_item", using: :btree
+    t.index ["order_id"], name: "index_shopr_order_items_on_order_id"
+    t.index ["ordered_item_id", "ordered_item_type"], name: "index_shopr_order_items_ordered_item"
   end
 
-  create_table "shopr_orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_orders", force: :cascade do |t|
     t.string   "token"
     t.string   "first_name"
     t.string   "last_name"
@@ -136,18 +136,18 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.datetime "accepted_at"
     t.datetime "shipped_at"
     t.integer  "delivery_service_id"
-    t.decimal  "delivery_price",                          precision: 8, scale: 2
-    t.decimal  "delivery_cost_price",                     precision: 8, scale: 2
-    t.decimal  "delivery_tax_rate",                       precision: 8, scale: 2
-    t.decimal  "delivery_tax_amount",                     precision: 8, scale: 2
+    t.decimal  "delivery_price",            precision: 8, scale: 2
+    t.decimal  "delivery_cost_price",       precision: 8, scale: 2
+    t.decimal  "delivery_tax_rate",         precision: 8, scale: 2
+    t.decimal  "delivery_tax_amount",       precision: 8, scale: 2
     t.integer  "accepted_by"
     t.integer  "shipped_by"
     t.string   "consignment_number"
     t.datetime "rejected_at"
     t.integer  "rejected_by"
     t.string   "ip_address"
-    t.text     "notes",                     limit: 65535
-    t.boolean  "separate_delivery_address",                                       default: false
+    t.text     "notes"
+    t.boolean  "separate_delivery_address",                         default: false
     t.string   "delivery_name"
     t.string   "delivery_address1"
     t.string   "delivery_address2"
@@ -155,18 +155,18 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.string   "delivery_address4"
     t.string   "delivery_postcode"
     t.integer  "delivery_country_id"
-    t.decimal  "amount_paid",                             precision: 8, scale: 2, default: "0.0"
-    t.boolean  "exported",                                                        default: false
+    t.decimal  "amount_paid",               precision: 8, scale: 2, default: "0.0"
+    t.boolean  "exported",                                          default: false
     t.string   "invoice_number"
     t.integer  "customer_id"
-    t.datetime "created_at",                                                                      null: false
-    t.datetime "updated_at",                                                                      null: false
-    t.index ["delivery_service_id"], name: "index_shopr_orders_on_delivery_service_id", using: :btree
-    t.index ["received_at"], name: "index_shopr_orders_on_received_at", using: :btree
-    t.index ["token"], name: "index_shopr_orders_on_token", using: :btree
+    t.datetime "created_at",                                                        null: false
+    t.datetime "updated_at",                                                        null: false
+    t.index ["delivery_service_id"], name: "index_shopr_orders_on_delivery_service_id"
+    t.index ["received_at"], name: "index_shopr_orders_on_received_at"
+    t.index ["token"], name: "index_shopr_orders_on_token"
   end
 
-  create_table "shopr_payments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_payments", force: :cascade do |t|
     t.integer  "order_id"
     t.decimal  "amount",            precision: 8, scale: 2, default: "0.0"
     t.string   "reference"
@@ -178,11 +178,11 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.boolean  "exported",                                  default: false
     t.datetime "created_at",                                                null: false
     t.datetime "updated_at",                                                null: false
-    t.index ["order_id"], name: "index_shopr_payments_on_order_id", using: :btree
-    t.index ["parent_payment_id"], name: "index_shopr_payments_on_parent_payment_id", using: :btree
+    t.index ["order_id"], name: "index_shopr_payments_on_order_id"
+    t.index ["parent_payment_id"], name: "index_shopr_payments_on_parent_payment_id"
   end
 
-  create_table "shopr_product_attributes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_product_attributes", force: :cascade do |t|
     t.integer  "product_id"
     t.string   "key"
     t.string   "value"
@@ -191,66 +191,66 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.boolean  "public",     default: true
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
-    t.index ["key"], name: "index_shopr_product_attributes_on_key", using: :btree
-    t.index ["position"], name: "index_shopr_product_attributes_on_position", using: :btree
-    t.index ["product_id"], name: "index_shopr_product_attributes_on_product_id", using: :btree
+    t.index ["key"], name: "index_shopr_product_attributes_on_key"
+    t.index ["position"], name: "index_shopr_product_attributes_on_position"
+    t.index ["product_id"], name: "index_shopr_product_attributes_on_product_id"
   end
 
-  create_table "shopr_product_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_product_categories", force: :cascade do |t|
     t.string   "name"
     t.string   "permalink"
-    t.text     "description",                  limit: 65535
+    t.text     "description"
     t.integer  "parent_id"
     t.integer  "lft"
     t.integer  "rgt"
     t.integer  "depth"
     t.string   "ancestral_permalink"
-    t.boolean  "permalink_includes_ancestors",               default: false
-    t.datetime "created_at",                                                 null: false
-    t.datetime "updated_at",                                                 null: false
-    t.index ["lft"], name: "index_shopr_product_categories_on_lft", using: :btree
-    t.index ["permalink"], name: "index_shopr_product_categories_on_permalink", using: :btree
-    t.index ["rgt"], name: "index_shopr_product_categories_on_rgt", using: :btree
+    t.boolean  "permalink_includes_ancestors", default: false
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+    t.index ["lft"], name: "index_shopr_product_categories_on_lft"
+    t.index ["permalink"], name: "index_shopr_product_categories_on_permalink"
+    t.index ["rgt"], name: "index_shopr_product_categories_on_rgt"
   end
 
-  create_table "shopr_product_categorizations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_product_categorizations", force: :cascade do |t|
     t.integer "product_id",          null: false
     t.integer "product_category_id", null: false
-    t.index ["product_category_id"], name: "categorization_by_product_category_id", using: :btree
-    t.index ["product_id"], name: "categorization_by_product_id", using: :btree
+    t.index ["product_category_id"], name: "categorization_by_product_category_id"
+    t.index ["product_id"], name: "categorization_by_product_id"
   end
 
-  create_table "shopr_products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_products", force: :cascade do |t|
     t.integer  "parent_id"
     t.string   "name"
     t.string   "sku"
     t.string   "permalink"
-    t.text     "description",       limit: 65535
-    t.text     "short_description", limit: 65535
-    t.boolean  "active",                                                  default: true
-    t.decimal  "weight",                          precision: 8, scale: 3, default: "0.0"
-    t.decimal  "price",                           precision: 8, scale: 2, default: "0.0"
-    t.decimal  "cost_price",                      precision: 8, scale: 2, default: "0.0"
+    t.text     "description"
+    t.text     "short_description"
+    t.boolean  "active",                                    default: true
+    t.decimal  "weight",            precision: 8, scale: 3, default: "0.0"
+    t.decimal  "price",             precision: 8, scale: 2, default: "0.0"
+    t.decimal  "cost_price",        precision: 8, scale: 2, default: "0.0"
     t.integer  "tax_rate_id"
-    t.boolean  "featured",                                                default: false
-    t.text     "in_the_box",        limit: 65535
-    t.boolean  "stock_control",                                           default: true
-    t.boolean  "default",                                                 default: false
-    t.datetime "created_at",                                                              null: false
-    t.datetime "updated_at",                                                              null: false
-    t.index ["parent_id"], name: "index_shopr_products_on_parent_id", using: :btree
-    t.index ["permalink"], name: "index_shopr_products_on_permalink", using: :btree
-    t.index ["sku"], name: "index_shopr_products_on_sku", using: :btree
+    t.boolean  "featured",                                  default: false
+    t.text     "in_the_box"
+    t.boolean  "stock_control",                             default: true
+    t.boolean  "default",                                   default: false
+    t.datetime "created_at",                                                null: false
+    t.datetime "updated_at",                                                null: false
+    t.index ["parent_id"], name: "index_shopr_products_on_parent_id"
+    t.index ["permalink"], name: "index_shopr_products_on_permalink"
+    t.index ["sku"], name: "index_shopr_products_on_sku"
   end
 
-  create_table "shopr_settings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_settings", force: :cascade do |t|
     t.string "key"
     t.string "value"
     t.string "value_type"
-    t.index ["key"], name: "index_shopr_settings_on_key", using: :btree
+    t.index ["key"], name: "index_shopr_settings_on_key"
   end
 
-  create_table "shopr_stock_level_adjustments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_stock_level_adjustments", force: :cascade do |t|
     t.integer  "item_id"
     t.string   "item_type"
     t.string   "description"
@@ -259,20 +259,20 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.integer  "parent_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-    t.index ["item_id", "item_type"], name: "index_shopr_stock_level_adjustments_items", using: :btree
-    t.index ["parent_id", "parent_type"], name: "index_shopr_stock_level_adjustments_parent", using: :btree
+    t.index ["item_id", "item_type"], name: "index_shopr_stock_level_adjustments_items"
+    t.index ["parent_id", "parent_type"], name: "index_shopr_stock_level_adjustments_parent"
   end
 
-  create_table "shopr_tax_rates", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_tax_rates", force: :cascade do |t|
     t.string   "name"
-    t.decimal  "rate",                       precision: 8, scale: 2
-    t.text     "country_ids",  limit: 65535
+    t.decimal  "rate",         precision: 8, scale: 2
+    t.text     "country_ids"
     t.string   "address_type"
-    t.datetime "created_at",                                         null: false
-    t.datetime "updated_at",                                         null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
-  create_table "shopr_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopr_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -287,8 +287,8 @@ ActiveRecord::Schema.define(version: 20160608183749) do
     t.string   "last_name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_shopr_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_shopr_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_shopr_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_shopr_users_on_reset_password_token", unique: true
   end
 
 end
