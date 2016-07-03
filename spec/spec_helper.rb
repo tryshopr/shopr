99333bc259abd93d71a4275f27dca2aff2c0c1c9
Dean Perry <dean@voupe.com>
2016-07-03 20:43:27 +0100

    require shoulda-matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require 'rspec/rails'
 require 'factory_girl_rails'
+require 'shoulda-matchers'
 
 # Load support files
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
